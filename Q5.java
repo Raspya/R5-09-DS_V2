281e178e661720a4938834c0fb79cee1ec1a4722
Matthias Bernouy <matt.bernouy@orange.fr>
2024-11-20 14:41:26 +0100

    Modif * to X

--- Q5.java
+++ Q5.java
@@ -2,7 +2,7 @@ public class Q5 {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         for (int i = 1; i <= n; i++) {
-            System.out.println("*".repeat(i));
+            System.out.println("X".repeat(i));
         }
     }
 }
