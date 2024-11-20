cd1c832231c9b3e53f54e13eda861d1ad98069b1
Matthias Bernouy <matt.bernouy@orange.fr>
2024-11-20 14:41:51 +0100

    inverser pyramide

--- Q5.java
+++ Q5.java
@@ -1,7 +1,7 @@
 public class Q5 {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        for (int i = 1; i <= n; i++) {
+        for (int i = n; i <= 1; i--) {
             System.out.println("X".repeat(i));
         }
     }
