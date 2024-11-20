3f645a35a9a09b4560af71bc0de72a42669029b5
MatthiasB <43455193+Raspya@users.noreply.github.com>
2024-11-20 14:58:16 +0100

    Update Q6.java

--- Q6.java
+++ Q6.java
@@ -2,6 +2,7 @@ public class Q6 {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         for (int i = n; i <= 1; i--) {
+            
             System.out.println("X".repeat(i));
         }
     }
