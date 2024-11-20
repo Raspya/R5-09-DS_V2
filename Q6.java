d65875b27b3063f57dc2af543c8488a9d7302c46
Matthias Bernouy <matt.bernouy@orange.fr>
2024-11-20 14:42:41 +0100

    Q6

---
+++ Q6.java
@@ -0,0 +1,8 @@
+public class Q6 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        for (int i = n; i <= 1; i--) {
+            System.out.println("X".repeat(i));
+        }
+    }
+}
