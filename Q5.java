e753e7790020a561442c42a7a808cc0d8121831c
Matthias Bernouy <matt.bernouy@orange.fr>
2024-11-20 14:40:43 +0100

    Q5

---
+++ Q5.java
@@ -0,0 +1,8 @@
+public class Q4 {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        for (int i = 1; i <= n; i++) {
+            System.out.println("*".repeat(i));
+        }
+    }
+}
